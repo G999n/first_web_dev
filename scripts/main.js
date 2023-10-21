108a42f52f888e2ad0c8ca3c84ac05cc714964c9
Gourav Kumar Das <gouravkumardas4@gmail.com>
2023-10-21 13:23:34 +0530

    second commit

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+    myImage.setAttribute("src", "images/firefox3.png");
   } else {
     myImage.setAttribute("src", "images/firefox.png");
   }
